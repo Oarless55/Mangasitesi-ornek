e8c12a5f516b3433f19321a0e1c91b66249211c2
Manga Sitesi <mangasitesi@example.com>
2026-03-03 18:27:50 +0300

    fix(middleware): correctly return redirect to prevent NavigationDuplicated loop

--- madara-studio/middleware/auth.js
+++ madara-studio/middleware/auth.js
@@ -1,9 +1,15 @@
 export default ({ store, route, redirect }) => {
+  // If the user is authenticated
   if (store.getters['user/auth']) {
+    // And they visit the login page (root url)
     if (route.path === '/') {
-      redirect('/stories')
+      return redirect('/stories')
     }
-  } else if (route.path !== '/') {
-    redirect('/')
+  } else {
+    // If the user is NOT authenticated
+    // And they are NOT already on the login page (root url)
+    if (route.path !== '/') {
+      return redirect('/')
+    }
   }
 }
