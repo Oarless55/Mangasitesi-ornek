937cf40acdc4be9513a412b20def859d7a412eb9
Manga Sitesi <mangasitesi@example.com>
2026-03-09 22:31:59 +0300

    chore: sync express body parser limit with remote

--- app.js
+++ app.js
@@ -46,8 +46,8 @@ apolloServer.applyMiddleware({
 //  apply to all requests
 // app.use(limiter)
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ extended: false, limit: '50mb' }))
 app.use(cookieParser())
 // app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')))
